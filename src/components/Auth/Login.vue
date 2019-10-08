04c7de49f212ce2c9bf4187ada9a3e53444ac059
Saadman Soumique <sssoumo@live.com>
2019-10-08 23:19:27 +0600

    vuetify added

---
+++ src/components/Auth/Login.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-container>
+        <v-layout align-center justify-center>
+            <v-flex xs12 sm8 md4>
+                <v-form>
+                    <v-card>
+                        <v-toolbar>
+                            <v-toolbar-title>
+                                Login Form
+                            </v-toolbar-title>
+                        </v-toolbar>
+                    </v-card>
+                </v-form>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>>
+
+<script>
+export default {
+    
+}
+</script>>
