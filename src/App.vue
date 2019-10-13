3adf4111905bf6081824cf50fffedeef5c2a247c
Saadman Soumique <sssoumo@live.com>
2019-10-13 14:03:04 +0600

    list section committed

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <v-app>
-    <Signup/>
+    <Todo/>
   </v-app>
 </template>
 
 <script>
 //import Login from './components/Auth/Login';
-import Signup from './components/Auth/Signup';
+//import Signup from './components/Auth/Signup';
+import Todo from './components/Todo';
 
 export default {
   name: 'App',
   components: {
-    Signup,
+    Todo,
   },
   data: () => ({
     //
---
+++ src/components/Todo.vue
@@ -0,0 +1,33 @@
+<template>
+<div style="max-height: 97.5vh; height: 100%">
+<v-container fluid fill-height pl-auto pr-0 pb-0 pt-0>
+        <v-layout row>
+            <v-flex lg3 pr-2>
+                <Lists/>
+            </v-flex>
+            <v-flex lg6 pr-2 pl-2>
+                Tasks
+            </v-flex>
+            <v-flex lg3 pl2>
+                Options
+            </v-flex>
+        </v-layout>
+    </v-container>
+    <v-footer height="auto" color="indigo">
+        <v-layout justify-center>
+            <v-flex indigo white--text text-xs-center>
+                &copy;2019
+                <strong>Saadman Soumique</strong>
+            </v-flex>
+        </v-layout>
+    </v-footer>
+</div>
+</template>
+
+<script>
+import Lists from "./Lists";
+export default {
+    name: "todo",
+    components: { Lists }
+}
+</script>>
